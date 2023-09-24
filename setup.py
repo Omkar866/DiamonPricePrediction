967f3f83c63de304e3f60e457c150ab81c95b4bf
Omkar866 <omkarbalwade@gmail.com>
2023-09-24 18:29:58 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+def get_requirements(filepath:str)->List[str]:
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+        return requirements
+
+setup(
+    name="DiamondPricePrediction",
+    version='0.0.1',
+    author='Omkar Balwade',
+    author_email='omkarbalwade@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
